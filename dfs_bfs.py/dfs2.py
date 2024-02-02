a8131f4da3ad369210252c02ccd88665544b94c9
beeen <mhosk25@gmail.com>
2024-02-02 11:52:20 +0000

    24.2.2. commit

---
+++ dfs_bfs.py/dfs2.py
@@ -0,0 +1,35 @@
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end = ' ')
+
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+from collections import deque
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+        print(v, end = ' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+graph = [
+         [0],
+         [2, 3, 8],
+         [1, 7],
+         [1, 4, 5],
+         [3, 5],
+         [3, 4],
+         [7],
+         [2, 6, 8],
+         [1, 7]
+         ]
+
+visited = [False] * 9
+# dfs(graph, 1, visited)
+bfs(graph, 1, visited)
