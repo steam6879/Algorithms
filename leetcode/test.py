b0a33be436ae9056dad1e427a8c2228d24fc3a29
beeen <mhosk25@gmail.com>
2024-02-18 10:20:19 +0000

    24.2.18. commit

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,22 +1,54 @@
-class Solution:
-    def islandPerimeter(self, grid: List[List[int]]) -> int:
-        square = 0  # 네모의 갯수
-        commonLine = 0  # 겹치는 라인 수 (겹치는 라인이 있으면 네모 하나씩 두번을 빼줘야함)
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    square += 1
-                    # 젤 위 / 젤 왼쪽 / 나머지 따로 계산
-                    if i == 0 and j > 0 and grid[0][j-1] == 1:
-                        commonLine += 1
-                    elif j == 0 and i > 0 and grid[i-1][0] == 1:
-                        commonLine += 1
-                    elif i != 0 and j != 0:
-                        if grid[i-1][j] == 1: commonLine += 1
-                        if grid[i][j-1] == 1: commonLine += 1
-        return (square*4) - (commonLine*2)
-if __name__ == '__main__':
-    sol = Solution()
-    x = [1, 3]
-    print(sol.searchInsert(x, 2))
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return f"TreeNode({self.val}, {self.left}, {self.right})"
     
+def insertLevelOrder(arr, root, i, n):
+    # Base case for recursion
+    if i < n:
+        temp = TreeNode(arr[i])
+        root = temp
+
+        # insert left child
+        root.left = insertLevelOrder(arr, root.left, 2 * i + 1, n)
+
+        # insert right child
+        root.right = insertLevelOrder(arr, root.right, 2 * i + 2, n)
+    return root	
+
+
+
+# Definition for a binary tree node.
+from typing import Optional
+ 
+class Solution:
+    longest: int = 0
+ 
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def dfs(node: TreeNode) -> int:
+            if not node:
+                return 0
+            # 왼쪽, 오른쪽의 각 리프 노드까지 탐색
+            left = dfs(node.left)
+            right = dfs(node.right)
+ 
+            # 가장 긴 경로
+            self.longest = max(self.longest, left + right)
+            # 상태값
+            return max(left, right) + 1
+ 
+        dfs(root)
+        return self.longest
+
+if __name__ == '__main__':
+    arr = [5,4,8,11,None,13,4,7,2,None,None,None,1]
+    n = len(arr)
+    root = None
+    root = insertLevelOrder(arr, root, 0, n)
+    s = Solution()
+
+    print(s.diameterOfBinaryTree(root))
