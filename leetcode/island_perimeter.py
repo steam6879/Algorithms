0379d4914dfb81f28511b5f48baf839ad07f2eef
beeen <mhosk25@gmail.com>
2024-02-18 02:51:41 +0000

    24.2.18. commit

---
+++ leetcode/island_perimeter.py
@@ -0,0 +1,31 @@
+from typing import Optional, List
+
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        square = 0
+        commonList = 0
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    square += 1
+
+                    if i == 0 and j > 0 and grid[0][j - 1] == 1:
+                        commonList += 1
+                    elif j == 0 and i > 0 and grid[i - 1][0] == 1:
+                        commonList += 1
+
+                    elif i != 0 and j != 0:
+                        if grid[i][j - 1] == 1:
+                            commonList += 1
+                        
+                        if grid[i - 1][j] == 1:
+                            commonList += 1
+
+        return (square*4) - (commonList*2)
+                
+if __name__ == '__main__':
+    s = Solution()
+
+    grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+    print(s.islandPerimeter(grid))
--- leetcode/test.py
+++ leetcode/test.py
@@ -1,32 +1,20 @@
-from typing import List, Sequence
-
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        pl = 0
-        pr = len(nums) - 1
-        if len(nums) == 1:
-            if nums[0] < target:
-                return 1
-            else:
-                return 0
-        while True:
-            pc = (pl + pr) // 2
-            if nums[pc] == target:
-                return pc
-            elif nums[pc] < target:
-                pl = pc + 1
-            else:
-                pr = pc - 1
-                if pr == -1:
-                    return 0
-
-            if pl == pr:
-
-                if nums[pl] < target:
-                    return pl + 1
-                else:
-                    return pl
-                
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        square = 0  # 네모의 갯수
+        commonLine = 0  # 겹치는 라인 수 (겹치는 라인이 있으면 네모 하나씩 두번을 빼줘야함)
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    square += 1
+                    # 젤 위 / 젤 왼쪽 / 나머지 따로 계산
+                    if i == 0 and j > 0 and grid[0][j-1] == 1:
+                        commonLine += 1
+                    elif j == 0 and i > 0 and grid[i-1][0] == 1:
+                        commonLine += 1
+                    elif i != 0 and j != 0:
+                        if grid[i-1][j] == 1: commonLine += 1
+                        if grid[i][j-1] == 1: commonLine += 1
+        return (square*4) - (commonLine*2)
 if __name__ == '__main__':
     sol = Solution()
     x = [1, 3]
