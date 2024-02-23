69424594d9bf73052240d1b8966365d5b5247bcb
beeen <mhosk25@gmail.com>
2024-02-23 10:56:44 +0000

    24.2.23. commit

--- leetcode/subtree_of_another_tree.py
+++ leetcode/subtree_of_another_tree.py
@@ -27,6 +27,22 @@ class Solution:
             return dfs(root.left, subRoot) or dfs(root.right, subRoot)
 
         return dfs(root, subRoot)
+    
+class Solution:
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        if not s: 
+            return False
+        if self.isSameTree(s, t): 
+            return True
+        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p and q:
+            return p.val == q.val \
+                    and self.isSameTree(p.left, q.left) \
+                    and self.isSameTree(p.right, q.right)
+        
+        return p is q
 
 
 class Solution:
