d7ddfe7d80d93abc84cc1a190e3d7912cad18c08
beeen <mhosk25@gmail.com>
2024-02-24 11:49:10 +0000

    24.2.24. commit

--- leetcode/subtree_of_another_tree.py
+++ leetcode/subtree_of_another_tree.py
@@ -6,6 +6,7 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         def isIdentical(root, subRoot):
@@ -29,20 +30,23 @@ class Solution:
         return dfs(root, subRoot)
     
 class Solution:
-    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
-        if not s: 
-            return False
-        if self.isSameTree(s, t): 
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not subRoot:
             return True
-        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+        if not root:
+            return False
+        if identical(root, subRoot):
+            return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
 
-    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        if p and q:
-            return p.val == q.val \
-                    and self.isSameTree(p.left, q.left) \
-                    and self.isSameTree(p.right, q.right)
-        
-        return p is q
+    def identical(p, q):
+        if not p and not q:
+            return True
+        if not p or not q or p.val != q.val:
+            return False
+        return identical(p.left, q.left) and identical(p.right, q.right)
+
+        return False
 
 
 class Solution:
@@ -81,3 +85,32 @@ class Solution:
             return isEqual(root, subRoot) or dfs(root.left, subRoot) or dfs(root.right, subRoot)
         
         return dfs(root, subRoot)
+
+
+class Solution:
+    def __init__(self):
+        self.answer = False
+
+    def isSubtree(self, s, t):
+        if s.val == t.val and not self.answer:
+            self.answer = self.DFS(s, t)
+            if self.answer:
+                return self.answer
+
+        if s.left:
+            self.isSubtree(s.left, t)
+        if s.right:
+            self.isSubtree(s.right, t)
+
+        return self.answer
+
+    def DFS(self, s, p):
+        if not s and not p:
+            return True
+        if not s or not p:
+            return False
+        if s.val != p.val:
+            return False
+        return self.DFS(s.left, p.left) and self.DFS(s.right, p.right)
+
+
