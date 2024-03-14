4051fc7b8b37b743f1090512c84ba2c0380112ba
beeen <mhosk25@gmail.com>
2024-03-14 14:18:22 +0000

    24.3.14. commit

---
+++ leetcode/Assign Cookies.py
@@ -0,0 +1,24 @@
+from typing import Optional, List
+
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        gp, sp = 0, 0
+        g.sort()
+        s.sort()
+
+        while gp < len(g) or sp <len(s):
+            if g[gp] <= s[sp]:
+                gp += 1
+                sp += 1
+            
+            else:
+                sp += 1
+
+        return gp
+    
+if __name__ == '__main__':
+    sol = Solution()
+    g = [1,2,3]
+    s = [1,1]
+
+    print(sol.findContentChildren(g, s))
