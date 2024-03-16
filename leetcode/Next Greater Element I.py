abac429d4b36c3a335bd2deb3834841ae51126f0
beeen <mhosk25@gmail.com>
2024-03-16 07:55:52 +0000

    24.3.16. commit

--- leetcode/Next Greater Element I.py
+++ leetcode/Next Greater Element I.py
@@ -1,6 +1,18 @@
 from typing import Optional, List
 
-class Solution:
+class Solution: #correct ver.
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        ans = [-1] * len(nums1)  # Initialize ans with -1
+        
+        for i in range(len(nums1)):
+            j = nums2.index(nums1[i])
+            for k in range(j + 1, len(nums2)):
+                if nums2[k] > nums1[i]:  # Compare with nums1[i], not nums2[j]
+                    ans[i] = nums2[k]
+                    break
+        return ans
+
+class Solution: #initially incorrect ver.
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         ans = []
         for i in range(len(nums1)):
@@ -13,3 +25,11 @@ class Solution:
                 ans[i] = -1
 
         return ans
+    
+
+    
+if __name__ == '__main__':
+    s = Solution()
+    nums1 = [4,1,2]
+    nums2 = [1,3,4,2]
+    print(s.nextGreaterElement(nums1, nums2))
