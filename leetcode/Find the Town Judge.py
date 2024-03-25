f544e8285cbe887e764a47a8193e387850dabbe0
beeen <mhosk25@gmail.com>
2024-03-25 14:51:08 +0000

    24.3.25. commit

--- leetcode/Find the Town Judge.py
+++ leetcode/Find the Town Judge.py
@@ -4,10 +4,21 @@ class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         if n == 1:
             return trust[0][1]
-        judge = trust[0][1]
+        
+        m = {key for key in range(n)}
+        for i in range(trust):
+            a, b = trust[0], trust[1]
+            m[a] = m.get(a, []) + [b]
 
-        for i in range(len(trust)):
-            if trust[i][1] != judge:
-                return -1
+        
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        count = [0] * (n+1)
+        for (a, b) in trust:
+            count[a] -= 1
+            count[b] += 1
             
-        return judge
+        for i in range(1, len(count)):
+            if count[i] == n-1:
+                return i
+        return -1
--- leetcode/test.py
+++ leetcode/test.py
@@ -1,5 +1,8 @@
-import heapq
-
-heap = [4, 5, 8, 2]
-heapq.heapify(heap)
-print(heap[-1])
+trust = [[1,3],[1,4],[2,3],[2,4],[4,3]]
+n = 4
+m = {}
+for i in range(len(trust)):
+    a, b = trust[i][0], trust[i][1]
+    m[a] = m.get(a, []) + [b]
+    # m[b] = m.get(b, []) + [a]
+print(m)
