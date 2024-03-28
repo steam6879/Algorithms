37e70d1b99130a1b420060a459734281d98a7e4c
beeen <mhosk25@gmail.com>
2024-03-28 10:48:05 +0000

    24.3.28. commit

--- leetcode/Find if Path Exists in Graph.py
+++ leetcode/Find if Path Exists in Graph.py
@@ -1,13 +1,14 @@
 from typing import List
+from collections import defaultdict, deque
 
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
         neighbors = defaultdict(list)
-        for n1, n2 in edges:
-            neighbors[n1].append(n2)
-            neighbors[n2].append(n1)
+        for a, b in edges:
+            neighbors[a].append(b)
+            neighbors[b].append(a)
             
-        def dfs(node, end, seen):
+        def dfs(node, end, seen: set):
             if node == end:
                 return True
             if node in seen:
@@ -22,5 +23,25 @@ class Solution:
         
         seen = set()    
         return dfs(start, end, seen)
+    
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], start: int, end: int) -> bool:
+        neighbors = defaultdict(list)
+        for n1, n2 in edges:
+            neighbors[n1].append(n2)
+            neighbors[n2].append(n1)
+
+        que = deque([start])
+        seen = set([start])
+        while que:
+            node = que.popleft()        
+            if node == end:
+                return True            
+            for n in neighbors[node]:
+                if n not in seen:
+                    seen.add(n)
+                    que.append(n)
+
+        return False
     
     #https://velog.io/@limelimejiwon/Leetcode-1971.-Find-if-Path-Exists-in-Graph
