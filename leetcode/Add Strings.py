d50d8a94971fea2863bb327d10958efb79e8470b
beeen <mhosk25@gmail.com>
2024-04-06 14:50:46 +0000

    24.4.6. commit

--- leetcode/Add Strings.py
+++ leetcode/Add Strings.py
@@ -4,4 +4,18 @@ class Solution:
 
         if len(num1) < len(num2):
             num1 += '0' * (len(num2) - len(num1))
-            
+        else:
+            num2 += '0' * (len(num1) - len(num2))
+
+        result = ''
+        carry = 0
+
+        for i in range(len(num1)):
+            digitSum = int(num1[i]) + int(num2[i]) + carry
+            carry = digitSum // 10
+            result += str(digitSum % 10)
+
+        if carry > 0:
+            result += str(carry)
+
+        return result[::-1]
