23e9576789c62153cf80d9a1dd3a1a92c1a37217
beeen <mhosk25@gmail.com>
2024-04-07 12:16:27 +0000

    24.4.7. commit

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,29 +1,17 @@
+from typing import List
+
 class Solution:
-  def addStrings(self, num1: str, num2: str) -> str:
-    # Reverse the input strings to start adding from the least significant digits
-    num1 = num1[::-1]
-    num2 = num2[::-1]
-    # Pad the shorter input string with zeros
-    if len(num1) < len(num2):
-        num1 += '0' * (len(num2) - len(num1))
-    else:
-        num2 += '0' * (len(num1) - len(num2))
-    
-    result = ''
-    carry = 0
-    
-    # Loop through both strings, starting from the least significant digits
-    for i in range(len(num1)):
-        # Convert the current digit in each string to an integer and add them together
-        digit_sum = int(num1[i]) + int(num2[i]) + carry
-        # Determine the carry for the next iteration, if any
-        carry = digit_sum // 10
-        # Append the least significant digit of the sum to the result string
-        result += str(digit_sum % 10)
-    
-    # If there is a carry after the last iteration, append it to the result string
-    if carry > 0:
-        result += str(carry)
-    
-    # Reverse the result string to obtain the final sum
-    return result[::-1]
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        m = {}
+        ans = []
+        for num in nums:
+            m[num] = abs(target - num)
+
+        for key, value in m.items():
+            if key + value == target:
+                if key == value
+                ans.append(nums.index(key))
+                nums.remove(key)
+
+if __name__ == "__main__":
+    print(Solution.twoSum(Solution, nums=[2, 7, 11, 5], target=9))
