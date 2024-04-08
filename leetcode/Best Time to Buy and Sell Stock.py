319419d1c96743af5744641b60281ca8d027cd1f
beeen <mhosk25@gmail.com>
2024-04-08 11:54:12 +0000

    24.4.8. commit

---
+++ leetcode/Best Time to Buy and Sell Stock.py
@@ -0,0 +1,51 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minPrice = 10000
+        profit = 0
+
+        for price in prices:
+            minPrice = min(price, minPrice)
+            profit = max(profit, price - minPrice)
+
+        return profit
+
+
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         decendingPrices = sorted(prices)[::-1]
+#         ans = 0
+#         maxPrice = 0
+
+#         while decendingPrices:
+#             i = prices.index(decendingPrices.pop())
+#             minPrice = prices[i]
+#             maxPrice = max(maxPrice, max(prices[i:]))
+#             ans = max(ans, maxPrice - minPrice)
+
+#         return ans
+
+
+
+
+
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         left, right = 1, len(prices) - 2
+#         minPrice, maxPrice = prices[0], prices[-1]
+
+#         while left <= right:
+#             minPrice = min(prices[left], minPrice)
+#             left += 1
+            
+#             maxPrice = max(prices[right], maxPrice)
+#             right -= 1
+
+#         if minPrice > maxPrice:
+#             return 0
+#         else:
+#             return maxPrice - minPrice
+        
+if __name__ == '__main__':
+    print(Solution.maxProfit(Solution, prices=[3, 2, 6, 5, 0, 3]))
--- leetcode/test.py
+++ leetcode/test.py
@@ -1,17 +1,13 @@
-from typing import List
-
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        m = {}
-        ans = []
-        for num in nums:
-            m[num] = abs(target - num)
-
-        for key, value in m.items():
-            if key + value == target:
-                if key == value
-                ans.append(nums.index(key))
-                nums.remove(key)
-
-if __name__ == "__main__":
-    print(Solution.twoSum(Solution, nums=[2, 7, 11, 5], target=9))
+class Solution():
+    def maxProfit(self, prices:list)->int:
+        
+        price_min = 10000 # 최저 주식값
+        profit = 0 # 수익
+        
+        for price_current in prices:
+            # 현재 가격 - 최저값의 값과 수익의 값 중, 최대값을 수익값으로 보고 스왑함
+            price_min = min(price_current, price_min)
+            
+            # 현재 가격 - 최저값의 값과 수익의 값 중, 최대값을 수익값으로 보고 스왑함
+            profit = max(profit, price_current-price_min) 
+        return profit
