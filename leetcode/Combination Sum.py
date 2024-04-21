2ffad7396974df746bfd0a9f1e69266eb2edba5a
beeen <mhosk25@gmail.com>
2024-04-21 09:36:26 +0000

    24.4.21. commit

--- leetcode/Combination Sum.py
+++ leetcode/Combination Sum.py
@@ -2,16 +2,44 @@ from typing import List
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        candidates.sort()
-        ans = []
+        ans, nums = [], []
 
-        def findCombination(candidates: List, target: int):
-            if not candidates:
-                return ans
+        def dfs(start, total):
+            if total > target:
+                return None
             
-            target = target - candidates[-1]
-            if target == 0:
-                return ans
+            elif total == target:
+                ans.append(nums[:])
+
             else:
-                for candidate in reversed(candidates):
-                    
+                for i in range(start, len(candidates)):
+                    num = candidates[i]
+                    nums.append(num)
+                    dfs(i, total + num)
+                    nums.pop()
+
+        dfs(0, 0)
+        return ans
+    
+if __name__ == "__main__":
+    print(Solution.combinationSum(Solution, candidates=[2, 3, 5], target=8))
+
+# class Solution:
+#     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+#         candidates.sort()
+#         ans = []
+
+#         def findCombination(candidates: List, target: int):
+#             if not candidates:
+#                 return ans
+            
+#             target = target - candidates[-1]
+#             if target == 0:
+#                 ans.append([candidates[-1]])
+#                 return ans
+            
+#             else:
+#                 for i in reversed(range(len(candidates))):
+#                     if target - candidates[i] >= 0:
+#                         ans.append()
+#                         return findCombination(candidates[:i + 1], target)
