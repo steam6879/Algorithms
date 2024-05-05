a0a65ed3b7480c8f7ebc1a8d30f61cdf8a540a19
beeen <mhosk25@gmail.com>
2024-05-05 07:02:35 +0000

    24.5.5. commit

--- leetcode/Contiguous Array.py
+++ leetcode/Contiguous Array.py
@@ -1,20 +1,55 @@
 from typing import List
+from collections import Counter
+
+class Solution: #실패
+    def findMaxLength(self, nums: List[int]) -> int:
+        if len(nums) == 2:
+            if nums[0] != nums[1]:
+                return 2
+            else:
+                return 0
+
+
+        prefixSum = [0] * len(nums)
+        prefixSum[0] = -1 if nums[0] == 0 else 1
+
+        for i in range(1, len(nums)):
+            if nums[i] == 0:
+                prefixSum[i] = prefixSum[i - 1] - 1
+            else:
+                prefixSum[i] = prefixSum[i - 1] + 1
+
+        m = Counter(prefixSum)
+
+        maxKey = max(m, key = m.get)
+        i1 = prefixSum.index(maxKey)
+        i2 = len(prefixSum) - 1 - prefixSum[::-1].index(maxKey)
+
+        return i2 - i1
+
+
 
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        
-        prefixSum = [nums[0]]
-        for i in range(1, len(nums)):
-            prefixSum[i] = prefixSum[i - 1] + nums[i]
+        if len(nums) <= 1:
+            return 0
 
-        m = {}
-        for num in prefixSum:
-            if num in prefixSum:
-                m[num] += 1
+        maxLength = 0
+        m = {0: -1}
+        count = 0
 
+        for i, num in enumerate(nums):
+            if num == 0:
+                count -= 1
             else:
-                m[num] = 1
+                count += 1
 
-        maxKey = max(m, key = m.get)
-        index1 = nums.index(maxKey)
-        index2 = nums.index(maxKey)
+            if count in m:
+                maxLength = max(maxLength, i - m[count])
+            else:
+                m[count] = i
+
+        return maxLength
+
+if __name__ == "__main__":
+    print(Solution.findMaxLength(Solution, nums=[0, 0, 1, 0, 0, 0, 1, 1]))
--- leetcode/test.py
+++ leetcode/test.py
@@ -1,4 +1,20 @@
-nums = [1, 2, 3, 4, 5, 6, 7]
-temp = [1, 3, 4]
-nums = temp + nums
-print(nums)
+from typing import List
+from collections import Counter
+
+nums = [0, 0, 1, 0, 0, 0, 1, 1]
+prefixSum = [0] * len(nums)
+prefixSum[0] = -1 if nums[0] == 0 else 1
+
+for i in range(1, len(nums)):
+    if nums[i] == 0:
+        prefixSum[i] = prefixSum[i - 1] - 1
+    else:
+        prefixSum[i] = prefixSum[i - 1] + 1
+
+m = Counter(prefixSum)
+
+maxKey = max(m, key = m.get)
+i1 = prefixSum.index(maxKey)
+i2 = len(prefixSum) - 1 - prefixSum[::-1].index(maxKey)
+
+print(i1, i2)
