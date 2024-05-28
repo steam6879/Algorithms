a365a911960a08758022325a326c73c976cabe49
beeen <mhosk25@gmail.com>
2024-05-28 10:59:06 +0000

    24.5.28. commit

--- leetcode/Middle of the Linked List.py
+++ leetcode/Middle of the Linked List.py
@@ -22,3 +22,13 @@ class Solution:
             head = head.next
         
         return head
+
+class Solution(object):
+    def middleNode(self, head):
+        # While slow moves one step forward, fast moves two steps forward.
+        # Finally, when fast reaches the end, slow happens to be in the middle of the linked list.
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
--- leetcode/test.py
+++ leetcode/test.py
@@ -3,20 +3,22 @@ from typing import Optional
 
 # Definition for singly-linked list.
 class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-        m = set()
-
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        temp = head
+        
+        count = 0
+        
         while head:
-            if head in m:
-                return True
-            else:
-                m.add(head)
-                head = head.next
+            head = head.next
+            count += 1
 
-        return False
+        for _ in range(count // 2):
+            temp = temp.next
+
+        return temp
