94f398547578b852df7d07e60c817b6adec7d71d
beeen <mhosk25@gmail.com>
2024-06-02 07:24:46 +0000

    24.6.2. commit

--- leetcode/Swap Nodes in Pairs.py
+++ leetcode/Swap Nodes in Pairs.py
@@ -10,41 +10,6 @@ class ListNode:
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
-        if not head:
-            return None
-        elif not head.next:
-            return head
-
-        dummy = ListNode(0)
-        dummy.next = head
-        prev = dummy
-        prev.next = head
-        curr = head
-        
-        while curr and curr.next:
-            temp = curr.next
-            curr.next = curr.next.next
-            temp.next = curr
-            prev.next = temp
-
-            curr = curr.next.next
-            prev = prev.next
-
-        return dummy.next
-    
-
-# Definition for singly-linked list.
-from typing import Optional
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-
-class Solution:
-    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-
         if not head or not head.next:
             return head
 
