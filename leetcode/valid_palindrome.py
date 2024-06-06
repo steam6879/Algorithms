37e94289de674f639bcc4bf76103f844723f28d9
beeen <mhosk25@gmail.com>
2024-06-06 10:57:51 +0000

    24.6.6. commit

--- leetcode/valid_palindrome.py
+++ leetcode/valid_palindrome.py
@@ -1,16 +1,24 @@
 from collections import deque
 
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-				# 자료형 데크로 선언
-        strs: Deque = collections.deque() 
-            
+        # 자료형 데크로 선언
+        strs: deque = deque()
+
         for char in s:
             if char.isalnum():
                 strs.append(char.lower())
-                
+
         while len(strs) > 1:
             if strs.popleft() != strs.pop():
                 return False
-            
+
         return True
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        ans = [char.lower() for char in s if char.isalnum()]
+
+        return ans == ans[::-1]
