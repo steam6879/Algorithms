aada02ef87ecd55db8b2099754420a9812fa6e10
beeen <mhosk25@gmail.com>
2024-06-25 14:26:44 +0000

    24.6.25. commit

--- leetcode/Group Anagrams.py
+++ leetcode/Group Anagrams.py
@@ -1,12 +1,40 @@
 from typing import List
-from collections import Counter
+from collections import Counter, defaultdict
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groupedAnagrams = defaultdict(list)
+
+        for s in strs:
+            # Sort the characters of the string to create a key
+            sortedKey = tuple(sorted(s))
+            # Add the string to the corresponding list in the dictionary
+            groupedAnagrams[sortedKey].append(s)
+
+        # Return the values of the dictionary as a list of lists
+        return list(groupedAnagrams.values())
 
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         sCounters = [Counter(sCounter) for sCounter in strs]
+        strs = set(strs)
+        ans = []
 
-        for str in strs:
-            sCounters.append(Counter(str))
+        for sCounter in sCounters:
+            bundle = []
+            for str in strs:
+                if Counter(str) == sCounter:
+                    bundle.append(str)
+                    # strs.remove(str)
 
-        
+            ans.append(bundle)
+
+        ans = [list(sublist) for sublist in set(tuple(sorted(sub)) for sub in ans)]
+
+        return ans
+
+
+if __name__ == "__main__":
+    print(Solution.groupAnagrams(Solution, strs=["eat", "tea", "tan", "ate", "nat", "bat"]))
--- leetcode/test.py
+++ leetcode/test.py
@@ -1,4 +1,15 @@
 from collections import Counter
-strs = ["eat","tea","tan","ate","nat","bat"]
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
 sCounters = [Counter(sCounter) for sCounter in strs]
 print(sCounters)
+strs = set(strs)
+print(strs)
+
+ans1 = [['ate', 'tea', 'eat'], ['ate', 'tea', 'eat'], ['nat', 'tan'], ['ate', 'tea', 'eat'], ['nat', 'tan'], ['bat']]
+
+# 중복 제거를 위해 set을 이용하여 각 서브리스트를 집합으로 변환한 후 다시 리스트로 변환
+unique_sublists = [list(sublist) for sublist in set(tuple(sorted(sub)) for sub in ans1)]
+
+ans2 = unique_sublists
+
+print(ans2)
