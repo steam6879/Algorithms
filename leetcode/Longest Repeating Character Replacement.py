96086ff6ecb45bbd67acd16be149ba395bab06ca
beeen <mhosk25@gmail.com>
2024-06-29 08:46:59 +0000

    24.6.29. commit

---
+++ leetcode/Longest Repeating Character Replacement.py
@@ -0,0 +1,57 @@
+from collections import Counter
+
+
+class Solution(object):
+    def characterReplacement(self, s, k):
+        maxLength, maxCount = 0, 0
+        counters = Counter()
+
+        for i in range(len(s)):
+            counters[s[i]] += 1
+            maxCount = max(maxCount, counters[s[i]])
+
+            if maxLength - maxCount >= k:
+                counters[s[i - maxLength]] -= 1
+
+            else:
+                maxLength += 1
+
+        return maxLength
+
+if __name__ == "__main__":
+    print(Solution.characterReplacement(Solution, s="AABABBA", k=1))
+# print(characterReplacement("AABABBA", 1))  # Output: 4
+
+
+# class Solution:
+#     def characterReplacement(s, k):
+#         # Dictionary to keep track of the count of each letter
+#         count = {}
+#         maxLength = 0
+#         maxCount = 0  # The count of the most frequent character in the current window
+#         left = 0  # Left pointer for the sliding window
+    
+#         for right in range(len(s)):
+#             # Update the count for the current character
+#             count[s[right]] = count.get(s[right], 0) + 1
+#             # Update the count of the most frequent character
+#             maxCount = max(maxCount, count[s[right]])
+    
+#             # If the current window size minus the count of the most frequent character
+#             # is greater than k, shrink the window from the left
+#             if (right - left + 1) - maxCount > k:
+#                 count[s[left]] -= 1
+#                 left += 1
+    
+#             # Update the maximum length of the window seen so far
+#             maxLength = max(maxLength, right - left + 1)
+    
+#         return maxLength
+
+#     # print(characterReplacement("ABAB", 2))  # Output: 4
+#     print(characterReplacement("AABABBA", 1))  # Output: 4
+
+
+
+# if __name__ == "__main__":
+#     print(Solution.characterReplacement(Solution, s='AABABBAA', k=1))
--- leetcode/test.py
+++ leetcode/test.py
@@ -1,15 +1,14 @@
-from collections import Counter
-strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
-sCounters = [Counter(sCounter) for sCounter in strs]
-print(sCounters)
-strs = set(strs)
-print(strs)
-
-ans1 = [['ate', 'tea', 'eat'], ['ate', 'tea', 'eat'], ['nat', 'tan'], ['ate', 'tea', 'eat'], ['nat', 'tan'], ['bat']]
-
-# 중복 제거를 위해 set을 이용하여 각 서브리스트를 집합으로 변환한 후 다시 리스트로 변환
-unique_sublists = [list(sublist) for sublist in set(tuple(sorted(sub)) for sub in ans1)]
-
-ans2 = unique_sublists
-
-print(ans2)
+# Time Complexity :  O(n)
+# Space Complexity : O(1)
+class Solution(object):
+    def characterReplacement(self, s, k):
+        maxlen, largestCount = 0, 0
+        arr = collections.Counter()
+        for idx in range(len(s)):
+            arr[s[idx]] += 1
+            largestCount = max(largestCount, arr[s[idx]])
+            if maxlen - largestCount >= k:
+                arr[s[idx - maxlen]] -= 1
+            else:
+                maxlen += 1
+        return maxlen
