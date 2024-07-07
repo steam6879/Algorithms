0092a39eaeb132dee6966d577e91a5029c97ad36
beeen <mhosk25@gmail.com>
2024-07-07 06:52:41 +0000

    24.7.7. commit

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,24 +1,17 @@
-from typing import Optional
-
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
+from collections import deque
 class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
-            return True
-
-        elif not p or not q:
-            return False
-
-        elif p.val != q.val:
-            return False
-
-        else:
-            return self.isSameTree(p.left, q.left) \
-                and self.isSameTree(p.right, q.right)
+    def levelOrder(self, root):
+        if not root: return []
+        queue, res = deque([root]), []
+        
+        while queue:
+            cur_level, size = [], len(queue)
+            for i in range(size):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                cur_level.append(node.val)
+            res.append(cur_level)
+        return res
