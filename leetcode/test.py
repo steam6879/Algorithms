383cf9f6fd678df27065184f9f72c4a378357ab8
beeen <mhosk25@gmail.com>
2024-07-07 08:01:14 +0000

    24.7.7. commit

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,17 +1,19 @@
 from collections import deque
+
+
 class Solution:
-    def levelOrder(self, root):
-        if not root: return []
-        queue, res = deque([root]), []
-        
-        while queue:
-            cur_level, size = [], len(queue)
-            for i in range(size):
-                node = queue.popleft()
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-                cur_level.append(node.val)
-            res.append(cur_level)
-        return res
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root:
+            if root == p or root == q:
+                return root
+            
+            l = self.lowestCommonAncestor(root.left, p, q)
+            r = self.lowestCommonAncestor(root.right, p, q)
+            
+            if r and l:
+                return root
+            elif r:
+                return r
+            else:
+                return l
+출처: https://sofar-sogood.tistory.com/entry/LeetCode-236-Lowest-Common-Ancestor-of-a-Binary-Tree-Python [작심삼일:티스토리]
