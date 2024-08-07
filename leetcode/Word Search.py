ce4c2af2eb288af3b258868109f6cac6f0de2e5d
beeen <mhosk25@gmail.com>
2024-08-07 13:19:16 +0000

    24.8.7. commit

--- leetcode/Word Search.py
+++ leetcode/Word Search.py
@@ -5,15 +5,15 @@ class Solution:
         def backtrack(i, j, k):
             if k == len(word):
                 return True
-            
+
             # Check if out of bounds or cell does not match the word
             if i < 0 or i >= m or j < 0 or j >= n or board[i][j] != word[k]:
                 return False
-            
+
             # Save the current cell and mark it as visited
             temp = board[i][j]
             board[i][j] = ''
-            
+
             if backtrack(i+1, j, k+1) or backtrack(i-1, j, k+1) or backtrack(i, j+1, k+1) or backtrack(i, j-1, k+1):
                 return True
 
