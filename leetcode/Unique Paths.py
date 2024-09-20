ddd843d6aac6d947dfd37e9a54986e01309a9077
beeen <mhosk25@gmail.com>
2024-09-20 12:09:22 +0000

    24.9.20. commit

--- leetcode/Unique Paths.py
+++ leetcode/Unique Paths.py
@@ -1,5 +1,5 @@
 import math
-
+from functools import cache
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
@@ -16,3 +16,22 @@ class Solution:
 class Solution2:
     def uniquePaths(self, m: int, n: int) -> int:
         return math.factorial(m + n - 2) // (math.factorial(m - 1) * math.factorial(n - 1))
+
+
+class Solution3:
+    def uniquePaths(self, m: int, n: int) -> int:
+        @cache
+        def dfs(row, col):
+            if row == m - 1 and col == n - 1:
+                return 1
+
+            total = 0
+            if row + 1 < m:
+                total += dfs(row + 1, col)
+
+            if col + 1 < n:
+                total += dfs(row, col + 1)
+
+            return total
+
+        return dfs(0, 0)
