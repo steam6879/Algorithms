83e0c838e07a5cc5f364e4aa7977e750d4fcb5b8
beeen <mhosk25@gmail.com>
2024-09-22 07:36:54 +0000

    24.9.22. commit

--- leetcode/test2.py
+++ leetcode/test2.py
@@ -1,22 +1,18 @@
-from collections import defaultdict, deque
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        carry = 0
+        result = ''
 
+        a = list(a)
+        b = list(b)
 
-indegree = defaultdict(int)
-numCourses = 4
-prerequisites = [[1, 0], [2, 0], [3, 1], [3, 2]]
-que = []
-graph = defaultdict(list)
-for a, b in prerequisites:
-    graph[b].append(a)
-    indegree[a] += 1
+        while a or b or carry:
+            if a:
+                carry += int(a.pop())
+            if b:
+                carry += int(b.pop())
 
-for node in graph:
-    for neighbor in graph[node]:
-        indegree[neighbor] += 1
+            result += str(carry %2)
+            carry //= 2
 
-for node in graph:
-    if indegree[node] == 0:
-        que.append(node)
-
-print(graph)
-print(indegree)
+        return result[::-1]
