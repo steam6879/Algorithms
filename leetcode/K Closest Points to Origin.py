71a114df12afafca5eab69e58ffe62dfc19ed9f1
beeen <mhosk25@gmail.com>
2024-09-29 04:34:51 +0000

    24.9.29. commit

--- leetcode/K Closest Points to Origin.py
+++ leetcode/K Closest Points to Origin.py
@@ -3,14 +3,30 @@ import heapq
 
 
 class Solution:
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+
+        heap = []
+
+        for (x, y) in points:
+            distance = -(x*x + y*y)
+
+            if len(heap) == K:
+                heapq.heappushpop(heap, (distance, x, y))
+            else:
+                heapq.heappush(heap, (distance, x, y))
+
+        return [(x, y) for (distance, x, y) in heap]
+
+
+class Solution2:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         ans = []
         heap = []
 
         for x, y in points:
-            distance = x ** 2 + y ** 2
+            distanceance = x ** 2 + y ** 2
 
-            heapq.heappush(heap, (distance, [x, y]))
+            heapq.heappush(heap, (distanceance, [x, y]))
 
         for _ in range(k):
             ans.append(heapq.heappop(heap)[1])
--- leetcode/test.py
+++ leetcode/test.py
@@ -1,17 +1,15 @@
+import heapq
+
 class Solution:
-    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+        
         heap = []
-
-        for x, y in points:
-            distance = x**2 + y**2 # sqrt does not matter for the size
-
-            # use negative distance for max heap
-            # adding new distance, and popping out the largest distance
-            if len(heap) < k:
-                heapq.heappush(heap, (-distance, x, y))
+        
+        for (x, y) in points:
+            dist = -(x*x + y*y)
+            if len(heap) == K:
+                heapq.heappushpop(heap, (dist, x, y))
             else:
-                heapq.heappushpop(heap, (-distance, x, y))
-
-        return [[x, y] for _, x, y in heap]
-            
-            
+                heapq.heappush(heap, (dist, x, y))
+        
+        return [(x,y) for (dist,x, y) in heap]
