14e3628351b842845587e2dad2172fc49815214a
beeen <mhosk25@gmail.com>
2024-09-29 03:39:31 +0000

    24.9.29. commit

--- leetcode/roman_to_integer.py
+++ leetcode/roman_to_integer.py
@@ -1,21 +1,19 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        m = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        }
         ans = 0
+        m = {"I": 1,
+             "V": 5,
+             "X": 10,
+             "L": 50,
+             "C": 100,
+             "D": 500,
+             "M": 1000}
 
-        for i in range(len(s)):
-            if i < len(s) - 1 and m[s[i]] < m[s[i + 1]]:
-                ans -= m[s[i]]
+        for i, roman in enumerate(s):
+            if i + 1 < len(s) and m[roman] < m[s[i + 1]]:
+                ans -= m[roman]
 
             else:
-                ans += m[s[i]]
+                ans += m[roman]
 
         return ans
