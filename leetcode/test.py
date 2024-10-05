9939335d87045adbb92df3303fafa798b3e2d001
beeen <mhosk25@gmail.com>
2024-10-05 10:18:34 +0000

    24.10.5. commit

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,15 +1,1 @@
-class Solution(object):
-    def subsets2(self, nums):
-        res = []
-        nums.sort()
-
-        for i in range(1 << len(nums)):
-            tmp = []
-
-            for j in range(len(nums)):
-                if i & 1 << j:  # if i >> j & 1:
-                    tmp.append(nums[j])
-
-            res.append(tmp)
-
-        return res
+m = 
