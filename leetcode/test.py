f4bf07f3239f1ef0ce25cd2d65c46dbccef424cc
beeen <mhosk25@gmail.com>
2024-10-13 09:48:23 +0000

    24.10.13. commit

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,15 +1,27 @@
-    def nextPermutation(self, nums):
-        for i in range(len(nums)-1, 0, -1):
-            # find the index of the last peak
-            if nums[i - 1] < nums[i]:
-                nums[i:] = sorted(nums[i:])
-                
-                # get the index before the last peak
-                j = i - 1
-                
-                # swap the pre-last peak index with the value just large than it
-                for k in range(i, len(nums)):
-                    if nums[j] < nums[k]:
-                        nums[k], nums[j] = nums[j], nums[k]
-                        return nums
-        return nums.reverse()
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        def isDuplicated(nums: List[str])->bool:
+            num_set = set()
+            for num in nums:
+                if num == ".": continue
+                if num in num_set: return True
+                num_set.add(num)
+            return False
+        
+        for horiz_num in range(9):
+            horiz_line = board[horiz_num]
+            if isDuplicated(horiz_line): return False
+        
+        for verti_num in range(9):
+            verti_line = [x[verti_num] for x in board]
+            if isDuplicated(verti_line): return False
+ 
+        for x in range(0,9,3):
+            for y in range(0,9,3):
+                sqare = [*(n for n in board[x][y:y+3]),
+                *(n for n in board[x+1][y:y+3]),
+                *(n for n in board[x+2][y:y+3]),
+                ]
+                if isDuplicated(sqare): return False
+        
+        return True
